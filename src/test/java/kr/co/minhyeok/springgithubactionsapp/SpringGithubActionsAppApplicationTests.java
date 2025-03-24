f8a99461d4acfb90c99e75dcf2676247895b23f2
unknown <wkdalsgur5556@gmail.com>
2025-03-24 17:38:11 +0900

    commit 0.0.1

---
+++ src/test/java/kr/co/minhyeok/springgithubactionsapp/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.minhyeok.springgithubactionsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
